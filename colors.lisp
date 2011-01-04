a002c809288ddc9b452732506cbcc08121852f93
Michael Baker <michaeltbaker@gmail.com>
2011-01-03 22:20:45 -0500

    Add background colors

--- colors.lisp
+++ colors.lisp
@@ -1,10 +1,14 @@
 ; Examples: 
-; Print to screen - (format t "This will be green:~a This will be red:~a" (green-text "green") (red-text "red"))
-; Return a string - (format nil "This will be green:~a This will be red:~a" (green-text "green") (red-text "red"))
+; Print to screen        - (format t "This will be green:~a This will be red:~a" (green-text "green") (red-text "red"))
+; Return a string        - (format nil "This will be green:~a This will be red:~a" (green-text "green") (red-text "red"))
+; Using bakground colors - (format t "~a~a" (red-background (blue-text "test")) (yellow-background (green-text "test")))
 
 (defun ansi-csi ()
   (format nil "~a[" (code-char 27)))
-  
+
+(defun ansi-clear ()
+  (format nil "~a0m" (ansi-csi)))
+
 (defun ansi-fg-red ()
   (format nil "~a31m" (ansi-csi)))
 
@@ -43,9 +47,27 @@
   
 (defun ansi-bg-purple ()
   (format nil "~a45m" (ansi-csi)))
+
+(defun ansi-bg-clear ()
+  (format nil "~a49m" (ansi-csi)))
+
+(defun red-background (text)
+  (format nil "~a~a~a" (ansi-bg-red) text (ansi-bg-clear)))
+
+(defun black-background (text)
+  (format nil "~a~a~a" (ansi-bg-black) text (ansi-bg-clear)))
   
-(defun ansi-clear ()
-  (format nil "~a0m" (ansi-csi)))
+(defun blue-background (text)
+  (format nil "~a~a~a" (ansi-bg-blue) text (ansi-bg-clear)))
+  
+(defun yellow-background (text)
+  (format nil "~a~a~a" (ansi-bg-yellow) text (ansi-bg-clear)))
+  
+(defun green-background (text)
+  (format nil "~a~a~a" (ansi-bg-green) text (ansi-bg-clear)))
+
+(defun purple-background (text)
+  (format nil "~a~a~a" (ansi-bg-purple) text (ansi-bg-clear)))
   
 (defun red-text (text)
   (format nil "~a~a~a" (ansi-fg-red) text (ansi-fg-clear)))
